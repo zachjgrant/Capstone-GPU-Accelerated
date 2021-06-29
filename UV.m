d48d55a23dfaade1feb75e8533bbe65977afc2f9
Zachary Grant <zachjosephgrant@gmail.com>
2021-06-29 17:59:05 +0000

    Test of GPU

--- UV.m
+++ UV.m
@@ -12,14 +12,17 @@ T_Silica = .95; %Max UV Transmittance for Quartz Silica Sleeve
 t = .3; %Thickness of quartz sleeve (cm)
 alpha = log(T_water);
 I_0 = T_Silica * (P / (2 * R_L * pi * L)); %Initial Intensity (W/cm^2)
-x = R_L:.0001:R; %cm
+tic;
+x = R_L:1e-9:R; %cm
+%x = linspace(R_L, R, 1270000001);
+%tic;
 I_xout = I_0 * (R_L ./ x) .* exp(alpha * (x-R_L)); %Intensity for Outward Propogation (W/cm^2)
 I_wall = I_0 * (R_L / R) * exp(alpha * (R-R_L)); %Intensity at the Wall (W/cm^2)
 I_0back = refl_SS * I_wall; %Initial Intensity for Inward Propogation (W/cm^2)
 I_xback = I_0back * (R ./ x) .* exp(alpha * (abs(x-R))); %Intensity for Inward Propogation (W/cm^2)
 I_total = I_xout + I_xback; %Total Intensity (W/cm^2)
 I_mean = mean(I_total); %Average Intensity (W/cm^2) (Can assume this for turbulent flow)
-
+time = toc;
 
 UVD = .016; %UV Dosage Standard (J/cm^2)
 t = UVD / I_mean; %Time of treatment needed (s)
@@ -27,5 +30,5 @@ t = UVD / I_mean; %Time of treatment needed (s)
 A_c = (R^2 - R_L^2) * pi; %Chamber Area (cm^2)
 v = L / t; %Inlet Velocity (cm/s)
 Q = A_c * v * .015850323; %Flow Rate (gpm)
-
+fprintf('%5.5f\n', time);
 
